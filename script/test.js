f3e7f391d4feb469035766f81479ef67f9235edb
Morpheus5909 <hotois.mathys@gmail.com>
2025-06-17 11:46:36 +0200

    début javascript

--- script/test.js
+++ script/test.js
@@ -1,3 +1,5 @@
+  /*
+
 let targetNumber = Math.floor(Math.random()*100)+1;
 console.log(targetNumber);
 let userNumber = prompt('Devinez un nombre entre 1 et 100 :');
@@ -27,8 +29,42 @@ let lastname = prompt("Quel est votre nom?");
 alert(`Bonjour ${firstname} ${lastname}!`);
 */
 
+    let trigger = document.querySelector('#send-button');
+    let numberZone = document.querySelector('#user-number');
+    let displayZone = document.querySelector('#game-display');
+let targetNumber = Math.floor(Math.random()*100)+1;
+console.log(trigger, numberZone, displayZone, targetNumber);
 
 
 
+function compareNumber() {
+
+//console.log ('ça marche');
+    let userNumber = numberZone.value;
+    let result
+       //console.log(userNumber);
+
+
+
+
+if (userNumber==targetNumber) {
+result = 'gagné'
+}
+else if(userNumber > targetNumber) {
+        result = `C'est trop grand!`;
+    } else {
+        result = `C'est trop petit!`;
+    }
+let resultZone = document.createElement('p');
+resultZone.innerHTML = result;
+resultZone.style.padding = "10px"
+resultZone.classList.add("result-line")
+//console.log(resultZone)
+
+displayZone.appendChild(resultZone);
+
+}
+
+trigger.addEventListener('click', compareNumber);
 
 
